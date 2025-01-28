d1e6c58dfbf0bf98e0d504938c563ae3ce385be4
Sebastian Amyotte <71189225+SebastianAmyotte@users.noreply.github.com>
2025-01-27 18:03:59 -0600

    Swap URL strategies

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -26,14 +26,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "9d59c715db6c8e17edd19c50950658de
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
-"flutter_bootstrap.js": "7ab5341e1fa85a0020a0580a80a717b5",
+"flutter_bootstrap.js": "1281c359ed4351818b9eda5b89cca3f2",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "990fc980a3ee473b695492640541a4ef",
-"/": "990fc980a3ee473b695492640541a4ef",
-"main.dart.js": "0b07eb262e45a1e21c93d1fa1ceffafe",
+"index.html": "f5489eae1b36a8cfc8224924bbe4439d",
+"/": "f5489eae1b36a8cfc8224924bbe4439d",
+"main.dart.js": "dd9bc6d394c2b916b5aaa982578ddb89",
 "manifest.json": "6b07c4b2f988131e2bc4d8ea192d9f9a",
 "version.json": "b2631abf325d04c35e5dd0c8246a8c87"};
 // The application shell files that are downloaded before a service worker can
